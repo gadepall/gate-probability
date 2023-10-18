298237fec02ee71f6fe0db1516e062ccb0afceda
Guptam1y1nk <141262935+Guptam1y1nk@users.noreply.github.com>
2023-10-18 13:19:34 +0530

    Add files via upload

---
+++ 2023/ST/26/codes/simulation.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Probability density function (PDF)
+double pdf(double x) {
+    return 0.5 * exp(-fabs(x));
+}
+
+// Calculate the cumulative distribution function (CDF)
+double cdf(double x) {
+    if (x < 0) {
+        return 0.5 * exp(x);
+    } else {
+        return 0.5 * (2.0 - exp(-x));
+    }
+}
+
+// Calculate the inverse CDF (quantile function)
+double inverse_cdf(double u) {
+    if (u < 0.5) {
+        return log(2.0 * u);
+    } else {
+        return -log(2.0 - 2.0 * u);
+    }
+}
+
+int main() {
+    int num_samples = 10000;  // Number of random variables to generate
+    double random_vars[num_samples];  // Array to store random variables
+    double cdf_values[num_samples];   // Array to store generated CDF values
+    double theoretical_cdf_values[num_samples];  // Array to store theoretical CDF values
+
+    srand(time(NULL));  // Seed the random number generator
+
+    for (int i = 0; i < num_samples; i++) {
+        double u = (double)rand() / RAND_MAX;  // Generate a random number between 0 and 1
+        double random_var = inverse_cdf(u);
+        random_vars[i] = random_var;  // Store the random variable in the array
+        cdf_values[i] = cdf(random_var);  // Calculate and store the CDF value
+        theoretical_cdf_values[i] = cdf(random_var); // Calculate and store the theoretical CDF value
+    }
+
+    // Compare generated CDF values with theoretical CDF at multiple points
+    for (int i = 0; i < num_samples; i++) {
+        printf("Random Variable %d: %f (Generated CDF = %f, Theoretical CDF = %f)\n", i + 1, random_vars[i], cdf_values[i], theoretical_cdf_values[i]);
+    }
+
+    // Save random_vars to a .dat file
+    FILE *random_vars_file = fopen("random_vars.dat", "w");
+    if (random_vars_file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    for (int i = 0; i < num_samples; i++) {
+        fprintf(random_vars_file, "%f\n", random_vars[i]);
+    }
+
+    fclose(random_vars_file);
+
+    // Save cdf_values to a .dat file
+    FILE *cdf_file = fopen("cdf_values.dat", "w");
+    if (cdf_file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    for (int i = 0; i < num_samples; i++) {
+        fprintf(cdf_file, "%f\n", cdf_values[i]);
+    }
+
+    fclose(cdf_file);
+
+    // Save theoretical_cdf_values to a .dat file
+    FILE *theoretical_cdf_file = fopen("theoretical_cdf_values.dat", "w");
+    if (theoretical_cdf_file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    for (int i = 0; i < num_samples; i++) {
+        fprintf(theoretical_cdf_file, "%f\n", theoretical_cdf_values[i]);
+    }
+
+    fclose(theoretical_cdf_file);
+
+    return 0;
+}
+
